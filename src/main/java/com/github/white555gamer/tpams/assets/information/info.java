d0b00633bb68853c4cadbaa14c5697f1e722f470
WHITE555Gamer <102808924+WHITE555Gamer@users.noreply.github.com>
2022-11-15 18:21:04 +0900

    Delete src/main directory

--- src/main/java/com/github/white555gamer/tpams/assets/information/info.java
+++
@@ -1,7 +0,0 @@
-package com.github.white555gamer.tpams.assets.information;
-
-public class info {
-
-    public static String Version = "0.2.0-SNAPSHOT";
-
-}
--- src/main/java/com/github/white555gamer/tpams/assets/messages/tempmsgs.java
+++
@@ -1,4 +0,0 @@
-package com.github.white555gamer.tpams.assets.messages;
-
-public class tempmsgs {
-}
